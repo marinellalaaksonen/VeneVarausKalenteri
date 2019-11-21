6e3467840b291e8d4afc16cb12cc44a467f07b1f
Marinella Laaksonen <marinella.laaksonen@gmail.com>
2019-11-21 22:16:53 +0200

    lisätty etusivu näyttämään yhteenveto varausten määrästä

--- application/routes/home.py
+++ application/routes/home.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from application import app
+from application.models.reservation import Reservation
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", boats_reserved_2019 = Reservation.count_reserved_boats("2019-01-01 00:00:00", "2020-01-01 00:00:00"))
